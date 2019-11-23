3f0ff4b34e2415da2dfed05ec82cbd7b7b449169
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-23 14:35:32 +0100

    Create Struct_PatientList.c

---
+++ PG1_WiSe19/Struct_PatientList.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARR_LEN 50
+
+typedef struct{
+    char name[ARR_LEN];
+    char vorname[ARR_LEN];
+    char strasse[ARR_LEN];
+    int  plz;
+    char ort[ARR_LEN];
+
+}patient;
+
+
+
+int main()
+{
+    int i = 0, j;
+    patient pt[4];
+
+    printf("Bitte die Patientendaten eingeben.\n\n");
+
+  do{
+
+    printf("\nName des Patienten: ");
+    scanf("%s", pt[i].name);
+    printf("\nVorname des Patienten: ");
+    scanf("%s", pt[i].vorname);
+    printf("\nStrasse des Patienten: ");
+    scanf("%s", pt[i].strasse);
+    printf("\nPostleitzahl des Patienten: ");
+    scanf("%d", &pt[i].plz);
+    printf("\nOrt des Patienten: ");
+    scanf("%s", pt[i].ort);
+    printf("\n\n");
+
+
+    i++;
+  } while(i < 2);
+
+  for(j=0; j<2; j++){
+    printf("\n\tName: %s\n\tVorname: %s\n\tStrasse: %s\n\tPLZ: %d\n\tOrt: %s\n", pt[j].name, pt[j].vorname, pt[j].strasse, pt[j].plz, pt[j].ort);
+  }
+
+
+    return 0;
+}
